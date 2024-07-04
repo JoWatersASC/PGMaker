4ccf5e5053598950414572f4d67813f9f098307c
JoWatersASC <joshua.waters365@gmail.com>
2024-07-03 21:34:08 -0400

    fixed overloads not being in namespace

--- src/overloads.cpp
+++ src/overloads.cpp
@@ -1,14 +1,16 @@
-#include"../include/node.h"
+#include"..\include\node.h"
 
-using namespace pgm;
+namespace pgm{
 
-// bool operator<(const Node& a, const Node& b){
-//     if(a.name != b.name){
-//         return a.name < b.name;
-//     }
-//     return a.observation < n.observation;
-// }
+bool operator<(const Node& a, const Node& b){
+    if(a.name != b.name){
+        return a.name < b.name;
+    }
+    return a.state < b.state;
+}
 
-// bool operator==(const Node& a, const Node& b){
-//     return a.name == b.name && a.values == b.values;
-// }
+bool operator==(const Node& a, const Node& b){
+    return a.name == b.name && a.values == b.values;
+}
+
+}
